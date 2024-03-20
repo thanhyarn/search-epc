24273ba83d7ffcfcafa28db7c16286ba1ca1a45f
Thanh Yarn <93782548+thanhyarn@users.noreply.github.com>
2024-03-20 17:22:49 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { Input, Row, Col } from "antd";
+import data from "./data.json";
+
+const App = () => {
+  const [value, setValue] = useState("");
+  const [result, setResult] = useState("");
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      // Tìm kiếm name dựa trên ID nhập vào
+      const item = data.find((item) => item.id === value);
+      if (item) {
+        setResult(item.name); // Hiển thị name tương ứng nếu tìm thấy
+      } else {
+        setResult("Không tìm thấy tên cho ID này"); // Hiển thị thông báo nếu không tìm thấy ID
+      }
+    }
+  };
+
+  return (
+    <div style={{ padding: "20px", marginTop: "100px" }}>
+      <Row justify="center">
+        <Col>
+          <Input
+            style={{
+              width: "500px",
+              height: "50px",
+              border: "3px solid black",
+            }}
+            name="valie"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            placeholder="Nhập thông tin..."
+            onKeyPress={handleKeyPress}
+          />
+          <h1>{result}</h1> {/* Hiển thị kết quả tìm được */}
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default App;
